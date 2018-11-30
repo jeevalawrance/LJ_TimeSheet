a7ed107bb41af884430a5d61554923d59ae40de8
Jeeva <jeevalawrance@gmail.com>
2018-11-30 23:35:19 +0400

    new vc

---
+++ TimeSheet/TaskInput/TS_AddNewTaskViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TS_AddNewTaskViewController.swift
+//  TimeSheet
+//
+//  Created by Jeeva on 11/30/18.
+//  Copyright © 2018 Jeeva. All rights reserved.
+//
+
+import UIKit
+
+class TS_AddNewTaskViewController: UIViewController {
+
+    @IBOutlet weak var lblForceCount: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
